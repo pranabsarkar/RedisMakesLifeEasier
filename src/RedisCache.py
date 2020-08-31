1ce7f3015536c3e28a9842317d809fc99ccfa2b3
pranabsarkar <sarkarpranab66@gmail.com>
2020-08-31 22:31:55 +0530

    modified Redis Class

--- src/RedisCache.py
+++ src/RedisCache.py
@@ -11,19 +11,14 @@ class ServerlessCache:
         )
 
     def setData(self, cache_key, api):
-        # dumps is to convert json data to string because redis accept string data
         api = json.dumps(api)
-        # syntax to store response first param is key and second is value
         self.rds.set(cache_key, api)
-        # here is the expire time for cache data you can change the time(in seconds)
         self.rds.expire(cache_key, 618400)
 
     def get(self, cache_key):
         # check the key if exist
         cacheData = self.rds.get(cache_key)
         if cacheData:
-            # as we stored api response in string format
-            # we have to convert it into json data
             cacheData = cacheData.decode("utf-8")
             # cacheData = cacheData.replace("'", "\"")
             cacheData = json.loads(cacheData)
